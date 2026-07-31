7b308b321dbd50a54aa08e78823a752ab31b93b3
unknown <mohmmd.qb@gmail.com>
2026-07-31 16:34:29 +0330

    add number guessing game with difficulty levels and input validation

---
+++ 01_basics/number_game.py
@@ -0,0 +1,67 @@
+import random
+import string
+
+def select_number(level):
+    number = 0
+    if level == 1:
+        number = random.randint(0, 50)
+    if level == 2:
+        number = random.randint(0, 100)
+    if level == 3:
+        number = random.randint(0, 200)
+    return number
+
+
+while True:
+
+    while True:
+        number_guess = 0
+        difficulty = int(input("" \
+        "1/ easy (1-50) 10 chance \n2/ hard (1-100) 8 chance \n3/ impossible (1-200) 6 chance: "))
+        if difficulty in (1, 2, 3):
+            if difficulty == 1:
+                number_guess = 10
+            elif difficulty == 2:
+                number_guess = 8
+            elif difficulty == 3:
+                number_guess = 6
+            break
+        else:
+            print("please insert a number in range 1 - 3")
+
+    random_number = select_number(difficulty)
+    won = False
+    gussed = 0
+    while gussed < number_guess:
+        gussed += 1
+        while True:
+            user_input = input("Enter your guess: ")
+            if user_input.isdigit():
+                user_input = int(user_input)
+                break
+            else:
+                print("insert a number")
+
+            
+        if user_input == random_number:
+            print(f"you get it right at {gussed} guess")
+            won = True
+            break
+        elif abs(user_input - random_number) < 5:
+            print("it is burning")
+        elif 5 <= abs(user_input - random_number) < 15:
+            print("you are close")
+        elif 15 <= abs(user_input - random_number) < 25:
+            print("try harder")
+        else: 
+            print("not good")
+
+    if not won:
+        print("you lost")
+    play_again = input("do you wanna play again? (y/n)").strip().lower()
+    if play_again == "n":
+        break
+
+
+
+    
