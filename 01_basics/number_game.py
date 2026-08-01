77d696a25c9db222d2f4e095c8f42c5ddc871c03
unknown <mohmmd.qb@gmail.com>
2026-08-01 11:06:21 +0330

    add complete number guessing game

--- 01_basics/number_game.py
+++ 01_basics/number_game.py
@@ -14,17 +14,9 @@ Features:
 import random
 import string
 
-def select_number(level):
-    number = 0
-    if level == 1:
-        number = random.randint(0, 50)
-    if level == 2:
-        number = random.randint(0, 100)
-    if level == 3:
-        number = random.randint(0, 200)
-    return number
 
 
+<<<<<<< HEAD
 while True:
 
     while True:
@@ -47,34 +39,80 @@ while True:
     gussed = 0
     while gussed < max_atttempts:
         gussed += 1
+=======
+def choose_difficulty():
+>>>>>>> 8af5a90 (add complete number guessing game)
         while True:
-            user_input = input("Enter your guess: ")
-            if user_input.isdigit():
-                user_input = int(user_input)
-                break
+            number_guess = 0
+            random_number = 0
+            difficulty = (input("" \
+            "1/ easy (1-50) 10 chance \n2/ hard (1-100) 8 chance \n3/ impossible (1-200) 6 chance: "))
+            if difficulty.isdigit():
+                difficulty = int(difficulty)
+                if difficulty == 1:
+                    return 10, random.randint(0, 50)
+                elif difficulty == 2:
+                    return 8, random.randint(0, 100)
+                elif difficulty == 3:
+                    return 6, random.randint(0, 200)
+                else:
+                    print("please insert a number in range 1 - 3")
             else:
-                print("insert a number")
+                print("please insert a diggit")
 
-            
-        if user_input == random_number:
-            print(f"you get it right at {gussed} guess")
+def get_user_guess():
+    while True:
+        guesse = input("enter your guess: ")   
+        if guesse.isdigit():
+            guesse = int(guesse)
+            return guesse
+        else:
+            print("insert a number")    
+
+
+def play_game(repitition, number):   
+    won = False
+    counter = 0
+    while counter < repitition:
+        counter += 1
+        user_guess = get_user_guess()
+        if user_guess == random_number:
+            print(f"you get it right at {counter} guess")
             won = True
             break
-        elif abs(user_input - random_number) < 5:
+        elif abs(user_guess - random_number) < 5:
             print("it is burning")
-        elif 5 <= abs(user_input - random_number) < 15:
+        elif 5 <= abs(user_guess - random_number) < 15:
             print("you are close")
-        elif 15 <= abs(user_input - random_number) < 25:
+        elif 15 <= abs(user_guess - random_number) < 25:
             print("try harder")
         else: 
             print("not good")
 
     if not won:
-        print("you lost")
+         print("you lost")
+
+
+
+while True:
+    max_rep, random_number = choose_difficulty()
+    print(max_rep, random_number)
+    play_game(max_rep, random_number)
     play_again = input("do you wanna play again? (y/n)").strip().lower()
     if play_again == "n":
         break
 
 
 
+<<<<<<< HEAD
     
+=======
+
+
+
+
+
+
+
+    
+>>>>>>> 8af5a90 (add complete number guessing game)
